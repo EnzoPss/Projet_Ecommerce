4fc3ac746523313e401e781cc2616080928b887b
EnzoPss <enzopousse.06@gmail.com>
2025-11-10 11:38:10 +0100

    tests sur home

---
+++ src/main/kotlin/org/ldv/ecommerce_lepicerie/controller/MainController.kt
@@ -0,0 +1,20 @@
+package org.ldv.ecommerce_lepicerie.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class MainController (){
+
+
+    /**
+     * Méthode permettant d'afficher la page d'accueil de l'application.
+     * @return le chemin vers le template a partir du dossier ressources/templates (on ne marque pas le .html)
+     */
+    @GetMapping("/Ecommerce_Lepicerie")
+    fun home():String{
+        return "index"
+    }
+
+
+}
