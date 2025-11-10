ef7dd56fd29007584531b8ccaf3b41868647fd80
EnzoPss <enzopousse.06@gmail.com>
2025-11-10 08:52:22 +0100

    fichiers

---
+++ src/main/kotlin/org/ldv/ecommerce_lepicerie/EcommerceLepicerieApplication.kt
@@ -0,0 +1,11 @@
+package org.ldv.ecommerce_lepicerie
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EcommerceLepicerieApplication
+
+fun main(args: Array<String>) {
+    runApplication<EcommerceLepicerieApplication>(*args)
+}
---
+++ src/test/kotlin/org/ldv/ecommerce_lepicerie/EcommerceLepicerieApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.ldv.ecommerce_lepicerie
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EcommerceLepicerieApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
